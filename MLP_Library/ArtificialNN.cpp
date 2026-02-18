6babf89e2f84855350de63195b61baa42a0b1049
Kittel <142875697+Kittel-Watne@users.noreply.github.com>
2026-02-18 11:20:36 +0100

    Started working on train()

--- MLP_Library/ArtificialNN.cpp
+++ MLP_Library/ArtificialNN.cpp
@@ -1,6 +1,7 @@
 #include "ArtificialNN.h"
 
 #include "RNG.h"
+#include "../ActivationFunctions.h"
 
 ArtificialNN::ArtificialNN(int numberInput, int numberOutput, int numberHiddenLayer, int numberNeuronHiddenLayer,
                            int OutputLearningRate, double learningRate, ACTIVATION_FUNCTION af_HiddenLayer, ACTIVATION_FUNCTION af_OutputLayer)
@@ -43,6 +44,45 @@ ArtificialNN::ArtificialNN(int numberInput, int numberOutput, int numberHiddenLa
 
 std::vector<double> ArtificialNN::Train(std::vector<double> inputValues, std::vector<double> desiredOutput)
 {
+	//Assuming all hidden layers have the same amount of neurons
+	int neuronsPerHidden = numNPerHidden[0];
+	//Assuming all hidden layers use the same activation functions
+	ACTIVATION_FUNCTION hiddenActivation = activationFunctionHiddenLayer[0];
+
+	std::vector<double> preActivation(numNPerHidden.size() * neuronsPerHidden);
+
+
+	for (int i = 0; i < neuronsPerHidden; i++)
+	{
+		double tempSum = 0;
+		for (int iV = 0; i < inputValues.size(); iV++)
+		{
+			tempSum += weights[iV + i * numInputs] * iV;
+		}
+		tempSum += biases[i];
+
+		preActivation[i] = tempSum;
+	}
+	
+
+	for (int l = 1; l < neuronsPerHidden; l++)
+	{
+		//For each neuron in the layer
+		for (int n = 0; n < numNPerHidden[l]; n++)
+		{
+			double tempSum = 0;
+			//For each input, which means each previous neuron
+			for (int previousN = 0; previousN < numNPerHidden[l]; previousN++){
+				
+				double tempSum += (weights[l * neuronsPerHidden + previousN] * MLNN_KandA::Math::ActivationFunction(hiddenActivation, preActivation[(l - 1) * neuronsPerHidden + previousN]));
+				
+			}
+			tempSum += biases[l * neuronsPerHidden + n];
+
+			preActivation[l * neuronsPerHidden + n] = tempSum;
+		}
+	}
+
 
 	return std::vector<double>(numOutputs, 0 );
 }
