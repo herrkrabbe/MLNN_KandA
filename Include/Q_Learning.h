90bd53eb2f3060463bb4a060fc5e7c21037ef1f4
Alexander <alexander.lossius@gmail.com>
2026-03-04 12:04:41 +0100

    QLearning header

---
+++ Include/Q_Learning.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "ArtificialNN.h"
+#include <memory>
+#include <vector>
+
+namespace MLNN_KandA
+{
+
+class Replay
+{
+public:
+	std::vector<double> states;
+	double reward;
+
+	Replay(std::vector<double> inputs, double r)
+	{
+		for (auto& elm : inputs)
+		{
+			states.push_back(elm);
+		}
+		reward = r;
+	}
+};
+
+class Q_Learning
+{
+private:
+	std::shared_ptr<ArtificialNN> ann;
+
+	std::vector<std::shared_ptr<Replay>> replayMemory;			//memory - list of past actions and rewards
+	int mCapacity = 10000;							//memory capacity
+
+	float discount = 0.99f;							//how much future states affect rewards
+
+public:
+	Q_Learning(int mCapacity, float discount, std::shared_ptr<ArtificialNN> ann);
+
+	static std::vector<double> SoftMax(std::vector<double> values);
+
+	void AddReplayMemory(std::vector<double> states, double reward);
+
+	void ProcessQLearningWithTrainANN();
+
+};
+}
---
+++ Source/Q_Learning.cpp
@@ -0,0 +1,1 @@
+#include "Q_Learning.h"
